2718a4acd62f3db0b628759c05747876bc77576a
Yuriy Shaklak <yurijfedor178@gmail.com>
2023-09-15 08:24:29 +0300

    add clients section

--- slider.js
+++ slider.js
@@ -1,10 +1,22 @@
 $(document).ready(function () {
-  $(".slider").slick({
+  $(".slider-advantages").slick({
     slidesToShow: 4, // Кількість відображених слайдів одночасно
     slidesToScroll: 1, // Кількість слайдів, які прокручуються за один раз
     dots: false,
     prevArrow: $(".prev-button"), // Селектор кнопки "назад"
     nextArrow: $(".next-button"), // Селектор кнопки "вперед"
     infinite: true, // Безкінечна прокрутка слайдів
+  });
+});
+
+$(document).ready(function () {
+  $(".slider-clients").slick({
+    slidesToShow: 5, // Кількість відображених слайдів одночасно
+    slidesToScroll: 1, // Кількість слайдів, які прокручуються за один раз
+    dots: false,
+    prevArrow: $(".prev-clients"), // Селектор кнопки "назад"
+    nextArrow: $(".next-clients"), // Селектор кнопки "вперед"
+    infinite: true, // Безкінечна прокрутка слайдів
+    initialSlide: 0,
   });
 });
