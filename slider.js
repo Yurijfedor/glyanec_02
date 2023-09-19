13dfea4bdef6d7fec3ca766ef1886f0243eca2c4
Yuriy Shaklak <yurijfedor178@gmail.com>
2023-09-19 23:55:53 +0300

    add responsive

--- slider.js
+++ slider.js
@@ -12,7 +12,27 @@ $(document).ready(function () {
 $(document).ready(function () {
   $(".slider-clients").slick({
     slidesToShow: 5, // Кількість відображених слайдів одночасно
-    slidesToScroll: 1, // Кількість слайдів, які прокручуються за один раз
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1615, // Розмір екрану, на якому змінити параметри
+        settings: {
+          slidesToShow: 4, // Кількість слайдів на екранах розміру 1024px і менше
+        },
+      },
+      {
+        breakpoint: 1320, // Розмір екрану, на якому змінити параметри
+        settings: {
+          slidesToShow: 2, // Кількість слайдів на екранах розміру 768px і менше
+        },
+      },
+      {
+        breakpoint: 400, // Розмір екрану, на якому змінити параметри
+        settings: {
+          slidesToShow: 1, // Кількість слайдів на екранах розміру 768px і менше
+        },
+      },
+    ], // Кількість слайдів, які прокручуються за один раз
     dots: false,
     prevArrow: $(".prev-clients"), // Селектор кнопки "назад"
     nextArrow: $(".next-clients"), // Селектор кнопки "вперед"
